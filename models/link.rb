f537d6d8cddb9b7b7fdd6859512efb1a3abbef16
antonyCastineiras <antonycastineiras80@gmail.com>
2016-10-18 15:32:07 +0100

    added heroku configuration to link.rb

--- models/link.rb
+++ models/link.rb
@@ -13,6 +13,8 @@ class Link
 
 end
 # DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
-
+#Heroku configuration
+Sequel.connect(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 DataMapper.finalize
 DataMapper.auto_upgrade!
