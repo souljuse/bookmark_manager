bd645b3c01267cc7ed0a95e5ee540888bd097433
souljuse <oppo.irene@gmail.com>
2016-10-18 13:18:17 +0200

    Step 11: added chance to add a new link and tested

--- app.rb
+++ app.rb
@@ -1,16 +1,24 @@
 require 'sinatra/base'
-require './spec/spec_helper'
+require './models/link.rb'
 
 class BookmarkManager < Sinatra::Base
   get '/' do
     erb :index
   end
 
-  get '/links' do
-  	@links = Link.all
-  	erb :links
+  post '/links' do
+    Link.create(title: params[:title], url: params[:url])
+    redirect '/links'
   end
 
+  get '/links' do
+    @links = Link.all
+    erb :links
+  end
+
+  get '/links/new' do
+    erb :links_new
+  end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
---
+++ spec/features/creating_link_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+feature 'Create Link' do
+	scenario 'creates a new link to add to database' do
+		visit('/links/new')
+
+    fill_in :title , with: "Bbc"
+    fill_in :url , with: "www.bbc.co.uk"
+    click_button('Create Link')
+
+    visit('/links')
+		expect(page).to have_content('Bbc')
+	end
+end
