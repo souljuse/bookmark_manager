c4262fe5740969617fe1f678347460c1e7122840
antonyCastineiras <antonycastineiras80@gmail.com>
2016-10-18 10:34:14 +0100

    initial project setup and created capybara test

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+gem 'data_mapper'
+gem 'dm-postgres-adapter'
+gem 'capybara'
+gem 'rspec'
+gem 'sinatra'
+gem 'rspec-sinatra'
+gem 'rerun'
---
+++ spec/features/displaying_links_spec.rb
@@ -0,0 +1,12 @@
+feature 'Display Link' do
+	scenario 'visiting the homepage' do
+		Link.create(url: 'http://makersacademy.com', title: 'Makers Academy')
+		visit('/links')
+
+		expect(page.status_code).to eq 200
+
+		within 'ul#links' do
+			expect(page).to have_content('Makers Academy')
+		end
+	end
+end
