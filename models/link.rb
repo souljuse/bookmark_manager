06894f79b3076f78931207d0d9b04f85d96c65d1
souljuse <oppo.irene@gmail.com>
2016-10-20 10:35:03 +0200

    commit

--- models/link.rb
+++ models/link.rb
@@ -6,10 +6,10 @@ class Link
 
 	include DataMapper::Resource
 
-	property :id, Serial
-	property :title, String
-	property :url,  String
-	property :tag, 	String
+	property :id, 		Serial
+	property :title, 	String
+	property :url,  	String
+	property :tag, 		String
 
 
 end
