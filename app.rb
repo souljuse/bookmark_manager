f5a13ba3743c943600b68904887b5172c9990851
souljuse <oppo.irene@gmail.com>
2016-10-20 18:27:20 +0200

    Step 18: user sign up and encrypted password added

--- app.rb
+++ app.rb
@@ -5,8 +5,29 @@ require 'sinatra/base'
 require_relative 'data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
+  enable :sessions
+  set :session_secret, 'super secret'
+
   get '/' do
+    @users = User.all
     erb :index
+  end
+
+  get '/users/new' do
+    erb :'users/new'
+  end
+
+  post '/users' do
+    user = User.create(email: params[:email],
+                password: params[:password])
+    session[:user_id] = user.id
+    redirect('/')
+  end
+
+  helpers do
+   def current_user
+     @current_user ||= User.get(session[:user_id])
+   end
   end
 
   post '/links' do
@@ -15,7 +36,7 @@ class BookmarkManager < Sinatra::Base
       link.tags << Tag.create(name: tag)
     end
     link.save
-    redirect '/links'
+    redirect('/links')
   end
 
   get '/links' do
@@ -31,7 +52,7 @@ class BookmarkManager < Sinatra::Base
   get '/tags/:name' do
     tag = Tag.first(name: params[:name])
     @links = tag ? tag.links : []
-    erb :'links'
+    erb :links
   end
 
   # start the server if ruby file executed directly
---
+++ spec/features/user_sign_up_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+feature 'User sign up' do
+	scenario 'when user signs up, the User count increases by 1' do
+    expect { sign_up }.to change(User, :count).by(1)
+	end
+
+  scenario 'Page displays hello message to user' do
+    sign_up
+    expect(page).to have_content('Welcome, alice@example.com')
+  end
+
+  scenario 'The user and the email given match' do
+    sign_up
+    expect(User.first.email).to eq('alice@example.com')
+  end
+end
